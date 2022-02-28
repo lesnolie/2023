976d2d9f6183a35dcb57b2bcf51b85e7f048d504
Leslie <81410185+lesnolie@users.noreply.github.com>
2022-03-01 01:01:24 +0800

    Update __init__.py

--- daily/__init__.py
+++ daily/__init__.py
@@ -5,12 +5,15 @@ from .from_issues import get_info_from_issue_comments
 from .shanbay import get_shanbay_daily
 
 MY_STATUS_DICT_FROM_API = {
-    # TODO url
-    # "扇贝": {"daily_func": get_shanbay_daily, "url": MY_SHANBAY_URL, "unit_str": " (天)"},
+    TODO url
+    "扇贝": {"daily_func": get_shanbay_daily, "url": MY_SHANBAY_URL, "unit_str": " (天)"},
+    "词场": {"daily_func": get_cichang_daily, "url": "", "unit_str": " (天)"},
+    "番茄": {"daily_func": get_forst_daily, "url": "", "unit_str": " (个)"},
 }
 
 MY_STATUS_DICT_FROM_COMMENTS = {
     "俯卧撑": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
+    "深蹲": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
     "GTD": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
     "周记": {"daily_func": get_info_from_issue_comments, "unit_str": " (周)"},
     "早起": {"daily_func": get_info_from_issue_comments, "unit_str": " (天)"},
